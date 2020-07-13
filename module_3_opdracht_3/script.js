f1428b2cf77147afc2de464afaa6f53bf23413cc
Janneke van Kaam <65725956+JvanKaam@users.noreply.github.com>
2020-07-13 14:11:00 +0200

    module 3 opdracht 3

---
+++ module_3_opdracht_3/script.js
@@ -0,0 +1,24 @@
+const age = 18;
+
+
+if (age > 17) {
+    console.log("mag naar binnen");
+} else {
+    console.log("toegang geweigerd");
+}
+
+const isFemale = false;
+
+if (isFemale) {
+    console.log("mag naar binnen");
+} else { 
+    console.log("begone thot");
+}
+
+const driverStatus = "bob"; 
+
+if (driverStatus == "bob") {
+    console.log("fijne reis");
+} else {
+    console.log("imma call a cab");
+}
